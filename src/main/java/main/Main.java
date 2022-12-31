e421f236bc6858ae4f49b5f68e0cfa1a5a09a777
DrNarae <jjyyhh99@naver.com>
2022-12-31 18:43:23 +0900

    버그 해결
    결과슬롯 미리보기 버그

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,20 +1,40 @@
 package main;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
 	public static Plugin PLUGIN = null;
+	FileConfiguration config = null;
 	
 	@Override
 	public void onEnable() {
 		PLUGIN = Bukkit.getPluginManager().getPlugin("RandomWorld");
-		getServer().getPluginManager().registerEvents(new RandomItem(), this);
+		getConfigFile();
+		Bukkit.getPluginManager().registerEvents(new RandomItem(), this);
+		getLogger().info("RandomWorld");
 	}
 
 	@Override
 	public void onDisable() {
+		getLogger().info("RandomWorld");
+	}
+	
+	private void getConfigFile() {
+		config = this.getConfig();
+		/* 
+		 * 온 오프
+		 * 유저 개별 적용
+		 * 아이템 밴
+		 * 적용할 아이템
+		 * 타이머
+		 * 적용할 이벤트(줍기, / 양조기, / 인첸트테이블, / 작업대, 화로, 모루, 용광로, 숫돌, 석재절단기, 훈연기, 대장장이작업대, 베틀, 지도제작대, 상인, 플레이어작업대)
+		 * ㄴ '/' 으로 나눈 것은 클래스(이벤트)로 구분해서 나누기
+		 * */
+		config.addDefault("", true);
+		config.options().copyDefaults(true);
+		saveConfig();
 	}
 }
--- src/main/java/main/RandomItem.java
+++ src/main/java/main/RandomItem.java
@@ -18,89 +18,117 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 public class RandomItem implements Listener {
-	public static final NamespacedKey KEY = new NamespacedKey(Main.PLUGIN, "randomCheck");
-	public static final Material[] MATERIALS = Material.values().clone();
-	public static final HashMap<InventoryType, String> INV_TYPES = new HashMap<InventoryType, String>(){
+	public static final NamespacedKey KEY = new NamespacedKey(Main.PLUGIN, "randomStatus");
+	public static final Material[] MATERIALS = Material.values();
+	public static final HashMap<InventoryType, Integer> INV_TYPES = new HashMap<InventoryType, Integer>() {
 		private static final long serialVersionUID = -3707007329761881137L;
 
 		{
-			put(InventoryType.ANVIL, "ANVIL");
-			put(InventoryType.BLAST_FURNACE, "BLAST_FURNACE");
-			put(InventoryType.CARTOGRAPHY, "CARTOGRAPHY");
-			put(InventoryType.CRAFTING, "CRAFTING");
-			put(InventoryType.FURNACE, "FURNACE");
-			put(InventoryType.GRINDSTONE, "GRINDSTONE");
-			put(InventoryType.LOOM, "LOOM");
-			put(InventoryType.MERCHANT, "MERCHANT");
-			put(InventoryType.SMITHING, "SMITHING");
-			put(InventoryType.SMOKER, "SMOKER");
-			put(InventoryType.STONECUTTER, "STONECUTTER");
-			put(InventoryType.WORKBENCH, "WORKBENCH");
+//			put(InventoryType.ANVIL, "ANVIL");
+//			put(InventoryType.BLAST_FURNACE, "BLAST_FURNACE");
+//			put(InventoryType.CARTOGRAPHY, "CARTOGRAPHY");
+//			put(InventoryType.CRAFTING, "CRAFTING");
+//			put(InventoryType.FURNACE, "FURNACE");
+//			put(InventoryType.GRINDSTONE, "GRINDSTONE");
+//			put(InventoryType.LOOM, "LOOM");
+//			put(InventoryType.MERCHANT, "MERCHANT");
+//			put(InventoryType.SMITHING, "SMITHING");
+//			put(InventoryType.SMOKER, "SMOKER");
+//			put(InventoryType.STONECUTTER, "STONECUTTER");
+//			put(InventoryType.WORKBENCH, "WORKBENCH");
+			put(InventoryType.CRAFTING, 0);
+			put(InventoryType.WORKBENCH, 0);
+			put(InventoryType.STONECUTTER, 1);
+			put(InventoryType.ANVIL, 2);
+			put(InventoryType.BLAST_FURNACE, 2);
+			put(InventoryType.CARTOGRAPHY, 2);
+			put(InventoryType.FURNACE, 2);
+			put(InventoryType.GRINDSTONE, 2);
+			put(InventoryType.MERCHANT, 2);
+			put(InventoryType.SMITHING, 2);
+			put(InventoryType.SMOKER, 2);
+			put(InventoryType.LOOM, 3);
 		}
 	};
 	
-	public void changeRandomItem(ItemStack stack) {
+	/**
+	 * -ITEM STATUS-
+	 * 0 - NULL
+	 * 1 - INTEGRITY
+	 * 2 - READY
+	 * 3 - CHANGED
+	 **/
+	public int getItemStatus(ItemStack stack) {
 		if (stack != null) {
 			if (!stack.getType().equals(Material.AIR)) {
-				if (stack.getItemMeta() != null) {
-					ItemMeta meta = stack.getItemMeta();
+				ItemMeta meta = stack.getItemMeta();
+				if (meta != null) {
 					PersistentDataContainer tag = meta.getPersistentDataContainer();
-					if (!tag.has(KEY, PersistentDataType.STRING)) {
-						tag.set(KEY, PersistentDataType.STRING, "true");
-						stack.setItemMeta(meta);
-						int randIdx = (int)(Math.random() * MATERIALS.length);
-						System.out.println(stack.getType() + " => " + MATERIALS[randIdx]);
-						stack.setType(MATERIALS[randIdx]);
+					if (tag.has(KEY, PersistentDataType.STRING)) {
+						if (tag.get(KEY, PersistentDataType.STRING).equals("ready")) {
+							return 2;
+						} else if (tag.get(KEY, PersistentDataType.STRING).equals("changed")) {
+							return 3;
+						}
+					} else {
+						return 1;
 					}
 				}
 			}
+		}
+		return 0;
+	}
+	
+	/**
+	 * -Change Item Tag-
+	 * Must be used only when the return value of getItemStatus() is not 0.
+	 **/
+	public void changeTag(ItemStack stack, String value) {
+		ItemMeta meta = stack.getItemMeta();
+		PersistentDataContainer tag = meta.getPersistentDataContainer();
+		tag.set(KEY, PersistentDataType.STRING, value);
+		stack.setItemMeta(meta);
+	}
+	
+	public void prepareItem(ItemStack stack) {
+		int status = getItemStatus(stack);
+		if (status == 1) {
+			changeTag(stack, "ready");
+		}
+	}
+	
+	public void changeRandomItem(ItemStack stack) {
+		int status = getItemStatus(stack);
+		if (status == 1 || status == 2) {
+			changeTag(stack, "changed");
+			int randIdx = (int)(Math.random() * MATERIALS.length);
+			System.out.println(stack.getType() + " => " + MATERIALS[randIdx]);
+			stack.setType(MATERIALS[randIdx]);
 		}
 	}
 
 	@EventHandler
 	public void inventoryClick(InventoryClickEvent e) {
 		if (e.getSlotType().equals(InventoryType.SlotType.RESULT)) {
-			// 슬롯타입이 RESULT인 칸을 클릭했을 때,
 			// #가끔 적용 안됨 (모루, ) => 왜?
-			// #다른 아이템이 커서에 있는 상태에서 누르면 조합대에서 미리 볼 수있음 => 악용가능
-			changeRandomItem(e.getCurrentItem());
+			if (e.getCursor() != null && e.getCursor().getType().equals(Material.AIR)) {
+				System.out.println("try Change");
+				changeRandomItem(e.getCurrentItem());
+				
+			} else {
+				System.out.println("try ready - 1");
+				prepareItem(e.getCurrentItem());
+			}
 		} else if (e.getAction().equals(InventoryAction.COLLECT_TO_CURSOR)) {
-			// 아이템을 더블클릭해서 같은 종류의 아이템을 한꺼번에 모았을 때,
 			// #가끔 적용 안됨 (훈연기, ) => 왜?
 			InventoryType invType = e.getWhoClicked().getOpenInventory().getType();
 			if (INV_TYPES.get(invType) != null) {
-				int rawSlotID = -1;
-				switch (INV_TYPES.get(invType)) {
-					// result rawslot = 0
-					case "CRAFTING" :
-					case "WORKBENCH" :
-						rawSlotID = 0;
-						break;
-					// result rawslot = 1
-					case "STONECUTTER" :
-						rawSlotID = 1;
-						break;
-					// result rawslot = 2
-					case "ANVIL" :
-					case "BLAST_FURNACE" :
-					case "CARTOGRAPHY" :
-					case "FURNACE" :
-					case "GRINDSTONE" :
-					case "SMITHING" :
-					case "SMOKER" :
-					case "MERCHANT" :
-						rawSlotID = 2;
-						break;
-					// result rawslot = 3
-					case "LOOM" :
-						rawSlotID = 3;
-						break;
-				}
+				int rawSlotID = INV_TYPES.get(invType);
 				ItemStack resultStack = e.getWhoClicked().getOpenInventory().getItem(rawSlotID);
 				if (e.getCursor() != null) {
 					if (e.getCursor().getType().equals(resultStack.getType())) {
-						// #조합대에서 미리 볼 수있음. => 악용 가능성 있음
-						changeRandomItem(resultStack);
+						System.out.println("try ready - 2");
+						prepareItem(resultStack);
 					}
 				}
 			}
@@ -121,7 +149,7 @@ public class RandomItem implements Listener {
 	
 	@EventHandler
 	public void enchantItem(EnchantItemEvent e) {
-		// #가끔 적용 안됨 => 어째서?
+		// #가끔 적용 안됨 => 왜?
 		changeRandomItem(e.getItem());
 	}
 }
